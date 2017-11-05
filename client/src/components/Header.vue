1b0e68bd18448b82435df0ed43ca9df43bd62b70
Sang Seok Lim <sangseok.lim@gmail.com>
2017-11-05 12:58:36 +0900

    password hashing

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
     <v-toolbar fixed class="cyan" dark>
         <v-toolbar-title class="mr-4">
-          <span @click="navigateTo({name: 'root'})">
+          <span class="home"
+            @click="navigateTo({name: 'root'})">
             Tab Tracker
           </span>
         </v-toolbar-title>
@@ -10,10 +11,14 @@
         </v-toolbar-items>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-            <v-btn flat dark
-                @click="navigateTo({name: 'register'})">
-                Sign Up
-            </v-btn>
+          <v-btn flat dark
+            @click="navigateTo({name: 'login'})">
+            Login
+          </v-btn>
+          <v-btn flat dark
+            @click="navigateTo({name: 'register'})">
+            Sign Up
+          </v-btn>
         </v-toolbar-items>
     </v-toolbar>
 </template>
@@ -29,4 +34,10 @@
 </script>
       
 <style scoped>
+  .home {
+    cursor: pointer;
+  }
+  .home:hover {
+    color:antiquewhite
+  }
 </style>
--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,13 +1,16 @@
 module.exports = {
-    port: process.env.PORT || 8081,
-    db: {
-        database: process.env.DB_NAME || 'geoeventfilter',
-        user: process.env.DB_USER || 'geoeventfilter',
-        password: process.env.DB_PASSWORD || 'geoeventfilter',
-        options:{
-            dialect: process.env.DIALECT || 'sqlite',
-            host: process.env.HOST || 'localhost',
-            storage: './geoeventfilter.sqlite'
-        }
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'geoeventfilter',
+    user: process.env.DB_USER || 'geoeventfilter',
+    password: process.env.DB_PASSWORD || 'geoeventfilter',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './geoeventfilter.sqlite'
     }
+  },
+  authentication: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
+  }
 }
