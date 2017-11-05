247eb98e9bdfae48d5a21c74b59409102c59d366
Sang Seok Lim <sangseok.lim@gmail.com>
2017-11-05 13:04:23 +0900

    auto complete off in register page

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -13,6 +13,7 @@
           <br>
           <v-text-field
             label="Password"
+            type="password"
             v-model="password">
           </v-text-field>
           <br>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -6,15 +6,21 @@
         <v-toolbar-title>Register</v-toolbar-title>
         </v-toolbar>
           <div class="pl-4 pr-4 pt-2 pb-2">
-              <v-text-field
-              label="Email"
-              v-model="email"
-              ></v-text-field>
-              <br>
-              <v-text-field
-              label="Password"
-              v-model="password"
-              ></v-text-field>
+            <form
+              name="tab-tracker-form"
+              autocomplete="off">
+                <v-text-field
+                label="Email"
+                v-model="email"
+                ></v-text-field>
+                <br>
+                <v-text-field
+                label="Password"
+                v-model="password"
+                type="password"
+                autocomplete="new-password"
+                ></v-text-field>
+            </form>
               <br>
               <div class="error" v-html="error" />
               <br>
