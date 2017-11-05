1d9637b18d842c9b5ca7f7e874afc68c73de5546
Sang Seok Lim <sangseok.lim@gmail.com>
2017-11-05 10:53:31 +0900

    Toolbar + buf fixes

---
+++ client/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-toolbar fixed class="cyan" dark>
+        <v-toolbar-title class="mr-4">
+            Tab Tracker
+        </v-toolbar-title>
+        <v-toolbar-items>
+            <v-btn flat dark>Browse</v-btn>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+            <router-link to="register">
+                <v-btn flat dark>
+                    Sign Up
+                </v-btn>
+            </router-link>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+      
+<script>
+    export default{
+    }
+</script>
+      
+<style scoped>
+</style>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -2,26 +2,23 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
-        <v-toolbar flat dense class="cyon" dark>
+        <v-toolbar flat dense class="cyan" dark>
         <v-toolbar-title>Register</v-toolbar-title>
         </v-toolbar>
           <div class="pl-4 pr-4 pt-2 pb-2">
-              <h1> Register </h1>
-              <input
-                type="email"
-                name="email"
-                v-model="email"
-                placeholder="email" />
+              <v-text-field
+              label="Email"
+              v-model="email"
+              ></v-text-field>
               <br>
-              <input
-                type="password"
-                name="password"
-                v-model="password"
-                placeholder="password" />
+              <v-text-field
+              label="Password"
+              v-model="password"
+              ></v-text-field>
               <br>
               <div class="error" v-html="error" />
               <br>
-              <v-btn class="cyon"
+              <v-btn class="cyan"
                 @click="register"> Register </v-btn>
             </div>
       </div>
@@ -35,7 +32,7 @@
 import AuthenticationService from '@/services/AuthenticationService'
 
 export default {
-  name: 'HelloWorld',
+  name: 'Register',
   data () {
     return {
       email: '',
@@ -71,4 +68,5 @@ export default {
 .error {
   color: red
 }
+
 </style>
